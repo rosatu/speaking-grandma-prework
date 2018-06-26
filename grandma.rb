dda511d7d1b5e78c2b2087a5e1b11ea051393b58
Rosa Tu <rosalina.tu@gmail.com>
2018-06-26 14:51:42 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase = "hey G!")
   
-  if phrase.upcase == phrase
+  if phrase.upcase == phrase && phrase.reverse != "!AMDNARG UOY EVOL I"
     return "NO, NOT SINCE 1938!"
-  elsif phrase == 'I LOVE YOU GRANDMA!'  
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return 'I LOVE YOU TOO PUMPKIN!'   
   else 
     return "HUH?! SPEAK UP, SONNY!"
-    
+  end  
 end
